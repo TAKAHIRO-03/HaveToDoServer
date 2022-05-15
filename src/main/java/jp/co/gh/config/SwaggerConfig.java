8aed8d411c24d4ca3508b5dcd6cec8dc54d2ae85
TAKAHIRO-03 <1701010402qb@cyber-u.ac.jp>
2022-05-15 23:15:25 +0900

    Swaggerのデフォルト値を無視する設定、バリデーションのライブラリ追加

--- src/main/java/jp/co/gh/config/SwaggerConfig.java
+++ src/main/java/jp/co/gh/config/SwaggerConfig.java
@@ -2,19 +2,32 @@ package jp.co.gh.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+/**
+ * Swagger（API仕様書自動生成ライブラリ）の設定情報を定義するクラス
+ */
 @Configuration
+@Import(BeanValidatorPluginsConfiguration.class)
 public class SwaggerConfig {
+
+    /**
+     * Swaggerの各種設定をするBeanを定義。
+     *
+     * @return {@link springfox.documentation.spring.web.plugins.Docket} bean
+     */
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .useDefaultResponseMessages(false);
     }
 }
