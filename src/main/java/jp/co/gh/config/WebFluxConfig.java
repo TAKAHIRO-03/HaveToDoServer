3aff45551a8ec872a12d0ba335ad3034c4883384
TAKAHIRO-03 <1701010402qb@cyber-u.ac.jp>
2022-08-12 23:46:27 +0900

    CORSの設定、Dockerfileのリファクタ

--- src/main/java/jp/co/gh/config/WebFluxConfig.java
+++ src/main/java/jp/co/gh/config/WebFluxConfig.java
@@ -2,6 +2,7 @@ package jp.co.gh.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.ReactivePageableHandlerMethodArgumentResolver;
+import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.result.method.annotation.ArgumentResolverConfigurer;
 
@@ -9,14 +10,22 @@ import org.springframework.web.reactive.result.method.annotation.ArgumentResolve
  * {@inheritDoc}
  */
 @Configuration
-public class WebfluxConfig implements WebFluxConfigurer {
+public class WebFluxConfig implements WebFluxConfigurer {
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public void configureArgumentResolvers(ArgumentResolverConfigurer configurer) {
+    public void configureArgumentResolvers(final ArgumentResolverConfigurer configurer) {
         configurer.addCustomResolver(new ReactivePageableHandlerMethodArgumentResolver());
+    }
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8888")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE")
+                .allowedHeaders("Content-Type", "Origin", "Accept");
     }
 
 }
