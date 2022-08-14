70eb2598cad25bc0ce8ddfb6ca7cb59e869aaac5
TAKAHIRO-03 <1701010402qb@cyber-u.ac.jp>
2022-08-14 15:23:16 +0900

    LoggingFilter内の処理でPasswordはマスクするよう修正

--- src/main/java/jp/co/gh/filter/log/BodyCaptureRequest.java
+++ src/main/java/jp/co/gh/filter/log/BodyCaptureRequest.java
@@ -1,4 +1,4 @@
-package jp.co.gh.filter.payload;
+package jp.co.gh.filter.log;
 
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.server.reactive.ServerHttpRequest;
@@ -8,6 +8,7 @@ import reactor.core.publisher.Flux;
 import java.nio.charset.StandardCharsets;
 
 public class BodyCaptureRequest extends ServerHttpRequestDecorator {
+
     private final StringBuilder body = new StringBuilder();
 
     BodyCaptureRequest(final ServerHttpRequest delegate) {
@@ -19,14 +20,10 @@ public class BodyCaptureRequest extends ServerHttpRequestDecorator {
      */
     @Override
     public Flux<DataBuffer> getBody() {
-        return super.getBody().doOnNext(this::capture);
+        return super.getBody().doOnNext((x) -> this.body.append(StandardCharsets.UTF_8.decode(x.asByteBuffer())));
     }
 
-    private void capture(final DataBuffer buffer) {
-        this.body.append(StandardCharsets.UTF_8.decode(buffer.asByteBuffer()));
-    }
-
-    public String getFullBody() {
+    String getFullBody() {
         return this.body.toString();
     }
 
